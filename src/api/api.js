edd0a52fc2f58bc2e6420ee2ce4223a0443d784a
Michael Knutson <mrknutson44@gmail.com>
2022-11-16 12:43:08 -0900

    wrap up

--- src/api/api.js
+++ src/api/api.js
@@ -2,4 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "//loanpro-api.fly.dev/api/v1",
+  // baseURL: "http://localhost:8080/api/v1",
 });
+
+// If using locally correct baseURL to baseURL: "http://localhost:8080/api/v1",
